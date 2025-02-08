3db356e67befee3df467a878aa16a3377f636418
Daria Lavitskaya <d.lavitskaya@g.nsu.ru>
2025-02-08 21:35:22 +0700

    webapp

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Canvas from "./components/Canvas";
+import "./App.css";  // <-- Подключаем стили
+
+function App() {
+    return (
+        <div className="App">
+            <Canvas />
+        </div>
+    );
+}
+
+export default App;
